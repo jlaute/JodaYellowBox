682ce04b1db0cc24de77b7b6962f883cf97259a9
Jörg Lautenschlager <jl@solutiondrive.de>
2018-03-16 22:16:50 +0100

    use on-change callback instead of calling each one seperate

--- Resources/StateMachine/config.php
+++ Resources/StateMachine/config.php
@@ -27,20 +27,10 @@ return $config = [
     ],
     'callbacks' => [
         'after' => [
-            'on-approve' => [
-                'on' => 'approve',                     # call the callback on a specific transition
-                'do' => ['@joda_yellow_box.sql_state_logger', 'log'],  # will call the method of this Symfony service
-                'args' => ['object']
-            ],
-            'on-reject' => [
-                'on' => 'reject',                     # call the callback on a specific transition
-                'do' => ['@joda_yellow_box.sql_state_logger', 'log'],  # will call the method of this Symfony service
-                'args' => ['object']
-            ],
-            'on-reopen' => [
-                'on' => 'reopen',                     # call the callback on a specific transition
-                'do' => ['@joda_yellow_box.sql_state_logger', 'log'],  # will call the method of this Symfony service
-                'args' => ['object']
+            'on-change' => [
+                'on'   => ['approve', 'reject', 'reopen'],               # call the callback on a specific transition
+                'do'   => ['@joda_yellow_box.sql_state_logger', 'log'],  # will call the method of this Symfony service
+                'args' => ['object', 'event']
             ]
         ]
     ]
