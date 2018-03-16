b1bd51d65072724d0e593bc4d6f96f46dacec9a3
Daniel Hormess <dani.hormess@me.com>
2018-03-16 13:29:35 +0100

    - refactored TestKernel class to run functional tests

--- tests/Unit/bootstrap.php
+++ tests/Unit/bootstrap.php
@@ -1,25 +1,29 @@
 <?php
 
 use Shopware\Models\Shop\Shop;
+use Shopware\Models\Shop\Repository;
 
 require __DIR__ . '/../../../../../autoload.php';
 
 class TestKernel extends \Shopware\Kernel
 {
     /**
-     * Static method to start boot kernel without leaving local scope in test helper.
+     * Static method to start boot kernel without leaving local scope in test helper
      */
     public static function start()
     {
-        $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
         $kernel = new self('testing', true);
         $kernel->boot();
+
         $container = $kernel->getContainer();
         $container->get('plugins')->Core()->ErrorHandler()->registerErrorHandler(E_ALL | E_STRICT);
-        /** @var $repository \Shopware\Models\Shop\Repository */
+
+        /** @var $repository Repository */
         $repository = $container->get('models')->getRepository(Shop::class);
+
         $shop = $repository->getActiveDefault();
         $shop->registerResources();
+
         $_SERVER['HTTP_HOST'] = $shop->getHost();
     }
 
@@ -28,4 +32,5 @@ class TestKernel extends \Shopware\Kernel
         return __DIR__ . '/config.php';
     }
 }
+
 TestKernel::start();
