c16881f5f24135d45d4854cd833a5240a508eb0c
Daniel Hormess <dani.hormess@me.com>
2018-03-23 14:21:42 +0100

    - added ticket controller

---
+++ Controllers/Frontend/YellowBox.php
@@ -0,0 +1,64 @@
+<?php
+
+use SM\SMException;
+use SM\Factory\Factory;
+use JodaYellowBox\Components\Ticket\TicketManager;
+
+class Shopware_Controllers_Frontend_YellowBox extends Enlight_Controller_Action
+{
+    /**
+     * @var Factory
+     */
+    protected $stateManager;
+
+    /**
+     * @var TicketManager
+     */
+    protected $ticketManager;
+
+    public function init()
+    {
+        $this->stateManager = Shopware()->Container()->get('joda_yellow_box.sm.factory');
+        $this->ticketManager = Shopware()->Container()->get('joda_yellow_box.services.ticket_manager');
+    }
+
+    public function indexAction()
+    {
+        $this->view->loadTemplate('frontend/joda_yellow_box/yellow_box.tpl');
+    }
+
+    /**
+     * @return Enlight_View|Enlight_View_Default
+     */
+    public function transitionAction()
+    {
+        $this->view->loadTemplate('frontend/joda_yellow_box/yellow_box.tpl');
+
+        $ticketId = (int) $this->request->get('ticketId');
+        if ($this->ticketManager->existsTicket($ticketId) === false) {
+            return $this->view->assign([
+                'success' => false,
+                'error' => 'ticket does not exists'
+            ]);
+        }
+
+        $ticket = $this->ticketManager->getTicket($ticketId);
+        $ticketTransition = $this->request->get('ticketTransition');
+
+        try {
+            $stateMaschine = $this->stateManager->get($ticket);
+            $ticket->reopen($stateMaschine);
+            $stateMaschine->apply($ticketTransition);
+        } catch (SMException $ex) {
+            // Invalid transition state
+            return $this->view->assign([
+                'success' => false,
+                'error' => $ex->getMessage()
+            ]);
+        }
+
+        // Success
+        $this->getModelManager()->flush($ticket);
+        return $this->view->assign('success', true);
+    }
+}
